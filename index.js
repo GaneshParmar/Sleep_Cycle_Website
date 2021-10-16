94c662d4edf9a00cece8758c204f40513f14283f
Ganesh Parmar <80880253+GaneshParmar@users.noreply.github.com>
2021-10-16 18:30:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Global Variables
 var win_width=innerWidth;
-
+var last_month_sleep="";
+var average_sleep=0;
 // If local storage is not created
         
 if(localStorage.getItem("sleep_status")==null){
@@ -24,6 +25,19 @@ if(localStorage.getItem("sleep_status")==null){
 
 }
 
+if(localStorage.getItem("last_month_sleep_status")==null){
+    // Creating the empty array
+    // var lmonth_sleep=[];
+    var sleep_stat_lMonth={
+        "Avg_Sleep":"Not Recorded",
+        "L_Sleep":"Not Recorded",
+        "H_Sleep":"Not Recorded"
+    }
+    // lmonth_sleep.push(sleep_stat_lMonth);
+    // Inserting the empty value in local storage
+    localStorage.setItem("last_month_sleep_status",JSON.stringify(sleep_stat_lMonth));
+}
+var last_month_sleep_status=JSON.parse(localStorage.getItem("last_month_sleep_status"));
 // if date is equal to 1
 function localstorage_update() {
     // Coping the lat date sleep time
@@ -50,21 +64,66 @@ function localstorage_update() {
 // Selecting the input (Navbar)
 
 function select_nav(i){
-    document.querySelectorAll("#container_item")[0].classList.remove('active');
-    document.querySelectorAll("#container_item")[1].classList.remove('active');
+    
+    
     // document.querySelectorAll("#container_item")[2].classList.remove('active');
 
     var nav_selection=document.querySelectorAll('input')[i].value;
     
-    if(nav_selection=="curr_habits") {
-        document.querySelectorAll("#container_item")[0].classList.add('active');
+    if(nav_selection=="curr_habits" || i==0) {
+        document.querySelector(".Cur_habits").classList.add('active');
+        document.querySelector(".Feat").classList.remove('active');
+        // document.querySelectorAll("#container_item")[1].style.display="none";
+        // document.querySelectorAll("#container_item")[0].classList.add('active');
     }
     // if(nav_selection=="add_habit"){
     //     document.querySelectorAll("#container_item")[1].classList.add('active');
     // }
-    if(nav_selection=="feat"){
-        document.querySelectorAll("#container_item")[1].classList.add('active');
+    if(nav_selection=="feat" || i==1){
+        document.querySelector(".Cur_habits").classList.remove('active');
+        // document.querySelectorAll("#container_item")[1].style.display="flex";
+        document.querySelector(".Feat").classList.add('active');
     }
+}
+
+// function to get total sleep time
+function get_sleep_time(sleep_time,waking_time) {
+    // Total sleep time
+    // more info https://stackoverflow.com/questions/10804042/calculate-time-difference-with-javascript/27484203
+    return dif =(( new Date("1970-1-2 " + waking_time) - new Date("1970-1-1 " + sleep_time) ) / 1000 / 60 /60).toFixed(2);
+}
+
+
+// Function for calculating last month Average sleep
+function Average_Sleep(schedule,num) {
+    for(var i=0;i<num;i++){
+        if(i==0){
+            var sleep_time=get_sleep_time(schedule[31]["last_day_month_sleep"],schedule[i][0]);
+        }
+        else{
+            var sleep_time=get_sleep_time(schedule[i-1][1],schedule[i][0]);
+        }
+        var sleep_h_l=get_l_h_slp_time(sleep_time,num);
+        average_sleep=average_sleep+parseInt(sleep_time);
+    }
+    // sleep_l_m["L_Sleep"]=sleep_h_l["l"];
+    // sleep_l_m["H_Sleep"]=sleep_h_l["h"];
+    // var average_sleep =;
+    console.log(average_sleep);
+    average_sleep=average_sleep/num;
+    return {"1": (parseInt(average_sleep)).toString()+":"+((parseInt(average_sleep))-((parseInt(average_sleep*60))).toString()),
+    "2":sleep_h_l["l"],"3":sleep_h_l["h"]};
+}
+var l_slp_time=10;
+var h_slp_time=5;
+function get_l_h_slp_time(t_sleep_time,num) {
+    if(h_slp_time<t_sleep_time){
+        h_slp_time=t_sleep_time;
+    }
+    if(l_slp_time>t_sleep_time){
+        l_slp_time=t_sleep_time
+    }
+    return {"l":l_slp_time,"h":h_slp_time};
 }
 
 // Function for form slide
@@ -123,7 +182,6 @@ function Update_stats(date,time,awake){
         // more info https://stackoverflow.com/questions/10804042/calculate-time-difference-with-javascript/27484203
         var dif =(( new Date("1970-1-2 " + time) - new Date("1970-1-1 " + sleep_time) ) / 1000 / 60 /60).toFixed(2);
         
-        //console.log(dif);
         var hour_slept=parseInt(dif);
         var minute_slept=parseInt((dif-parseInt(dif))*60);
 
@@ -132,7 +190,7 @@ function Update_stats(date,time,awake){
             minute_slept="0"+minute_slept;
         }
         var slept_time=hour_slept+":"+minute_slept
-        //console.log(slept_time);
+        // console.log(slept_time);
 
         // Storing the dif and sleep_time in localstorage
         schedule[no_][2]=dif+"@"+slept_time;
@@ -192,7 +250,7 @@ function Activate_btn(){
         if(time==5 || time==6|| time==7 && morning){
             update_btn.style.opacity="1";
             update_btn.style.pointerEvents="all";
-            update_btn.innerHTML="I Woke Up<img src='Images/woke_up.png' width='40' height='30'>";
+            update_btn.innerHTML="I Woke Up<img src='./Images/woke_up.png' width='40' height='30'>";
             update_btn.addEventListener('click',function () {
                 Update_stats(today_date,current_hrn_min_time,true);
                 work_btn_show();
@@ -202,10 +260,10 @@ function Activate_btn(){
             work_btn_show();
         }
         if(time==22 || time==23 && night){
-            //console.log("I am in");
+            // console.log("I am in");
             update_btn.style.opacity="1";
             update_btn.style.pointerEvents="all";
-            update_btn.innerHTML="Now Sleep<img src='Images/sleep.png' width='40' height='30'>";
+            update_btn.innerHTML="Now Sleep<img src='./Images/sleep.png' width='40' height='30'>";
             update_btn.addEventListener('click',function () {
                 Update_stats(today_date,current_hrn_min_time,false);
                 work_btn_show(true);
@@ -249,17 +307,23 @@ function show_sleep_schedule(ele){
     var date_selected=ele.innerText;
     var schedule_date=schedule[date_selected-1];
     var sleep_stat=schedule_date[2].split("@");
-    //console.log(sleep_stat);
+    // console.log(sleep_stat);
     var total_sleep=sleep_stat[1];
     var sleep_health=sleep_stat[0];
-    //console.log(`I slept ${schedule[date_selected-1][1]} on date ${date_selected}`);
+    // console.log(`I slept ${schedule[date_selected-1][1]} on date ${date_selected}`);
     // Selecting the doiv to shoe the sleep hour of particular date
     var sleep_stat_show=document.getElementById("sleep_hour");
+    if (date_selected==today_date){
+        var header="Today Sleep Cycle";
+    }
+    else{
+        var header=`My Sleep Schedule On Date <span class='date'>${date_selected}/${month}/${year}</span>`;
+    }
     sleep_stat_show.innerHTML=`        
-        <h5>${date_selected==today_date?'Todays Sleep Cycle': `My Sleep Schedule On Date <span class="date">${date_selected}/${month}/${year}</span>`}</h5>
-        <p>Waking Time <span class="special">${schedule_date[0]}</span></p>
-        <p>Sleeping Time <span class="special">${schedule_date[1]}</span></p>
-        <h3>Total Sleep ${total_sleep}</h3>
+        <h5>${header}</h5>
+        <p style="color:gray;">Waking Time <span class="special">${schedule_date[0]}</span></p>\
+        <p style="color:gray;">Sleeping Time <span class="special">${schedule_date[1]}</span></p>\
+        <h3 style="color:black; font-size:1.1em;">Total Sleep ${total_sleep}</h3>
     `;
 }
 
@@ -277,12 +341,12 @@ function highlight_date_bg(schedule) {
 
         // var sleep_health=sleep_stat[0];
         if(isNaN(total_sleep)){
-            // console.log(true);
+            console.log(true);
             date_.style.background="rgba(0, 0, 0, 0.026)";
         }
         else{
-            //console.log(j);
-            //console.log(typeof(total_sleep));
+            // console.log(j);
+            // console.log(typeof(total_sleep));
             if (6>total_sleep>=5) {
                 date_.style.background="#fcd403";
             };
@@ -347,13 +411,25 @@ function set_month() {
     }
     highlight_date_bg(schedule);
 
+    // Updating the last  month sleep status
+
+    if(last_date==today_date){
+
+        last_month_avg_l_h=Average_Sleep(schedule,last_date);
+        last_month_sleep_status["Avg_Sleep"]=last_month_avg_l_h["1"];
+        last_month_sleep_status["L_Sleep"]=last_month_avg_l_h["2"];
+        last_month_sleep_status["H_Sleep"]=last_month_avg_l_h["3"];
+        // console.log(last_month_sleep_status);
+        localStorage.setItem("local_month_sleep_status",JSON.stringify(last_month_sleep_status));
+        // location.reload();
+    }
 
     }
 // Close stats
 function close_(stat_id,blur) {
     var stat_container=document.getElementById(stat_id);
     var blur_bg=document.getElementById(blur);
-    //console.log(stat_container);
+    // console.log(stat_container);
 
     // Display none
     stat_container.style.display="none";
@@ -363,7 +439,7 @@ function close_(stat_id,blur) {
 function show_stat(stat_id,blur) {
     var stat_container=document.getElementById(stat_id);
     var blur_bg=document.getElementById(blur);
-    //console.log(stat_container);
+    // console.log(stat_container);
 
     // Display none
     stat_container.style.display="inline-block";
@@ -382,20 +458,45 @@ function clear_local(){
     location.reload();
 }
 
+// Last month sleep status update here
+function last_month_status_update(l_month) {
+    var l_month_slp=document.getElementById("l_month_stat");
+    var avg_sleep=last_month_sleep_status["Avg_Sleep"];
+    var l_sleep=last_month_sleep_status["L_Sleep"];
+    var h_sleep=last_month_sleep_status["H_Sleep"];
+    l_month_slp.innerHTML=`
+        <div class="month" id="month_name">${monthNames[l_month-1]}</div>
+        <div class="close" onclick="close_('l_month_stat','blurry_bg')">X</div>
+        <div class="l_month_info">
+            <h4>Average Sleep <span id="l_month_avg_slp">${avg_sleep}</span></h4>
+            <h5>Lowest Sleep <span id="l_month_l_slp">${l_sleep}</span></h5>
+            <h5>Highest Sleep <span id="l_month_h_slp">${h_sleep}</span></h5>
+        </div>
+        <footer>you see <i>last month states</i></footer>
+    `;
+}
 
 
 // Calling the function
 // If date is equal to 1
-if(today_date==1){ localstorage_update();}
+if(today_date==1){ localstorage_update();
+}
 select_nav(0);
 Activate_btn();
 set_month();
-
+// for(var i=0;i<31;i++){
+//     if(i<10){
+//         Update_stats(i+1,"5:30",true);
+//     }
+//     else{
+//         Update_stats(i+1,"5:00",true);
+//     }    
+// }
 // Getting the active_date element
 var active_date_ele=document.querySelector(".active_date");
 show_sleep_schedule(active_date_ele);
 
+last_month_status_update(month-1);
 
 
-// Update_stats(1,"5:30",true);
-// Update_stats(31,"22:00",false);
+// Update_stats(14,"22:00",false);
